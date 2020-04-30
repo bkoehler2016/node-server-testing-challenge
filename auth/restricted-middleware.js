9d009b1e001fc9586a2efc1a29732a05bba4fd5d
Ben Koehler <bens@Bens-MacBook-Pro.local>
2020-04-30 13:34:41 -0700

    updated database

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { jwtSecret } =require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization.split(" ")[1];
 
   if(token){
     jwt.verify(token, jwtSecret, (err, decodedToken) => {
